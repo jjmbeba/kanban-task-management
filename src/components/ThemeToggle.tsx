59920161afad5f59940975d0eb2d3c153b76679b
John Johnson Mbeba <110187869+jmbeba@users.noreply.github.com>
2023-12-27 15:49:40 +0300

    Fix iterator key

--- src/components/ThemeToggle.tsx
+++ src/components/ThemeToggle.tsx
@@ -23,6 +23,7 @@ export function ThemeToggle() {
             onClick={() => setTheme(themeValue)}
             value={themeValue}
             id={themeValue}
+            key={themeValue}
           />
         ))}
         <div
